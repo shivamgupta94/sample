e0c7ddf0df2ee20e8a55718667c4b3264063b5b3
Shivam Gupta <shivamgupta.me@gmail.com>
2014-11-26 21:43:30 +0530

    aarush_style

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Sample1
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   
+  config.assets.enabled = true
   #Required for Heroku
   config.assets.initialize_on_precompile = false
   end
