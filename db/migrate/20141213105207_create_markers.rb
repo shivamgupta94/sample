c8a07e1ff2112aab1aaf64f0b3d3da946ef41246
Shivam Gupta <shivamgupta.me@gmail.com>
2014-12-14 11:33:29 +0530

    changed scale of decimal

--- db/migrate/20141213105207_create_markers.rb
+++ db/migrate/20141213105207_create_markers.rb
@@ -1,8 +1,8 @@
 class CreateMarkers < ActiveRecord::Migration
   def change
     create_table :markers do |t|
-      t.decimal :lat, :precision => 20, :scale => 25
-      t.decimal :long, :precision => 20, :scale => 25
+      t.decimal :lat, :precision => 20, :scale => 19
+      t.decimal :long, :precision => 20, :scale => 19
       t.string :description
       t.references :rou, index: true
 
